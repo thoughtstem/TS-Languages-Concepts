64255d4962f5cf0610c8449191f3b31aa86b9db5
Joshua Gibson <joshalexgibs@gmail.com>
2019-09-05 11:41:01 -0700

    fixed dependencies for vr, data-sci, and memes

--- my-data-sci-lang/info.rkt
+++ my-data-sci-lang/info.rkt
@@ -5,6 +5,7 @@
 
 (define deps '(
   "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util"
+  "https://github.com/thoughtstem/TS-Languages.git?path=data-sci"
   ;If this language depends on another language, add that dep here
   ))
 
--- my-vr-lang/info.rkt
+++ my-vr-lang/info.rkt
@@ -5,6 +5,7 @@
 
 (define deps '(
   "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util"
+  "https://github.com/thoughtstem/vr-lang.git"
   ;If this language depends on another language, add that dep here
   ))
 
