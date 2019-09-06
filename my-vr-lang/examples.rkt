6958516642ba2175f9c81779df09791b6e78be3d
Joshua Gibson <joshalexgibs@gmail.com>
2019-09-06 12:19:59 -0700

    Changed my-vr-lang/examples.rkt; changed #lang from racket to vr-lang

--- my-vr-lang/examples.rkt
+++ my-vr-lang/examples.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang vr-lang
 
 (require ts-kata-util)
 
@@ -11,7 +11,7 @@
 ;VR Beginner Examples
 
 ;Write a function that produces a blue box on a black background.
-(define-example-code my-vr-lang/main shapes~box-001
+(define-example-code my-vr-lang shapes~box-001
  
   (define my-scene
     (scene
@@ -26,7 +26,7 @@
   (send-to-browser my-scene))
 
 ;Code a scene with a sphere
-(define-example-code my-vr-lang/main shapes~sphere-002
+(define-example-code my-vr-lang shapes~sphere-002
 
   (define my-scene
     (scene
@@ -40,7 +40,7 @@
   (send-to-browser my-scene))
 
 ;Code a scene with a large cylinder
-(define-example-code my-vr-lang/main shapes~size-003
+(define-example-code my-vr-lang shapes~size-003
 
   (define my-scene
     (scene
@@ -55,7 +55,7 @@
   (send-to-browser my-scene))
 
 ;Create a scene with a white sphere on a light blue background.
-(define-example-code my-vr-lang/main shapes~snowman-001
+(define-example-code my-vr-lang shapes~snowman-001
   
   (define (my-sphere)
     (sphere
@@ -73,7 +73,7 @@
   (send-to-browser my-scene))
 
 ;Create a scene with a snowman on a light blue background.
-(define-example-code my-vr-lang/main shapes~snowman-002
+(define-example-code my-vr-lang shapes~snowman-002
 
   (define (my-snowman n)
     (sphere
@@ -93,7 +93,7 @@
   (send-to-browser my-scene))
 
 ;Create a scene with a snowman and tree on a light blue background.
-(define-example-code my-vr-lang/main shapes~snowman-003
+(define-example-code my-vr-lang shapes~snowman-003
  
   (define (my-snowman n)
     (sphere
@@ -133,7 +133,7 @@
 
 
 ;Create a scene with a snowman and tree on a light blue background with snow.
-(define-example-code my-vr-lang/main shapes~snowman-004
+(define-example-code my-vr-lang shapes~snowman-004
   
   ;Here we declare the star-system component
 
